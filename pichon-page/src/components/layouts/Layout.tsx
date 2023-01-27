1fb736736b9ac6900879f9f02d50a883e83c01da
Sem Hernandez <73005584+sem-hm@users.noreply.github.com>
2023-01-26 18:23:21 -0600

    chore: Eliminar codigo que no se utiliza (#8)

--- pichon-page/src/components/layouts/Layout.tsx
+++ pichon-page/src/components/layouts/Layout.tsx
@@ -9,17 +9,10 @@ interface LayoutProps {
   // ? esto quiere decir que es opcional
 }
 
-const origin = typeof window === "undefined" ? "" : window.location.origin;
-
 export const Layout: FC<LayoutProps> = ({ children, title }) => {
-  const defaultDark =
-    typeof window === "undefined"
-      ? false
-      : window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [dark, setDark] = useState<boolean>(defaultDark);
 
   return (
-    <div className="container is-fluid" data-theme={dark ? "dark" : "light"}>
+    <div className="container is-fluid">
       <Head>
         <title>{title || "PC Studio"}</title>
         <meta name="author" content="Pichones Corp" />
@@ -34,15 +27,6 @@ export const Layout: FC<LayoutProps> = ({ children, title }) => {
         {/* <meta property="og:image" content={`${origin}/img/banner.png`} /> */}
       </Head>
       <Navbar />
-      <div className="field">
-        <input
-          type="checkbox"
-          defaultChecked={dark}
-          onChange={() => setDark(!dark)}
-        />
-        <label>Theme {dark}</label>
-      </div>
-
       <main>{children}</main>
     </div>
   );
